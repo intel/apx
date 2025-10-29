4b1dbebb544bb751c95c3d75048558a555cac7da
Chang S. Bae <chang.seok.bae@intel.com>
2025-10-29 14:17:54 -0700

    KVM: VMX: Refactor register index retrieval from exit qualification
    Introduce a helper to extract the register index from the VMX exit
    qualification field.
    
    In addition to the VMX instruction information field, exit qualification
    also encodes a register index. This field will expand into the previously
    reserved bit for extended register IDs. This refactoring will simplify
    the extended register handling without code duplication.
    
    Since the VMCS exit qualification is cached in VCPU state, the helper can
    reference it efficiently via vmx_get_exit_qual().
    
    No functional change intended.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    ---
    V2 -> V3: s/GPR/register (Sean)

--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -323,6 +323,11 @@ static __always_inline unsigned long vmx_get_exit_qual(struct kvm_vcpu *vcpu)
 	return vt->exit_qualification;
 }
 
+static inline int vmx_get_exit_qual_reg(struct kvm_vcpu *vcpu)
+{
+	return (vmx_get_exit_qual(vcpu) >> 8) & 0xf;
+}
+
 static __always_inline u32 vmx_get_intr_info(struct kvm_vcpu *vcpu)
 {
 	struct vcpu_vt *vt = to_vt(vcpu);
