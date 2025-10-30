dd03c4dd067b81242d50fec94c34f46160bed0e3
Chang S. Bae <chang.seok.bae@intel.com>
2025-10-30 12:09:27 -0700

    KVM: VMX: Support extended register index in exit handling
    Support 5-bit register indices in VMCS fields when APX feature is
    enumerated.
    
    The presence of the extended instruction information field is indicated
    by APX enumeration, regardless of the XCR0.APX bit setting.
    
    With APX enumerated, the previously reserved bit in the exit qualification
    can be referenced safely now. However, there is no guarantee that older
    implementations always zeroed this bit.
    
    Link: https://lore.kernel.org/7bb14722-c036-4835-8ed9-046b4e67909e@redhat.com
    Suggested-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    ---
    Changes since last version:
    * Switch the condition for using the extended instruction information
      from checking XCR0 to relying on APX enumeration (Paolo).
    * Rewrite the changelog to clarify this behavior.

--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -372,12 +372,26 @@ struct vmx_insn_info {
 	union insn_info info;
 };
 
-static inline struct vmx_insn_info vmx_get_insn_info(struct kvm_vcpu *vcpu __maybe_unused)
+/*
+ * The APX enumeration guarantees the presence of the extended fields.
+ * The host CPUID bit alone is sufficient to rely on it.
+ */
+static inline bool vmx_insn_info_extended(void)
+{
+	return static_cpu_has(X86_FEATURE_APX);
+}
+
+static inline struct vmx_insn_info vmx_get_insn_info(struct kvm_vcpu *vcpu)
 {
 	struct vmx_insn_info insn;
 
-	insn.extended  = false;
-	insn.info.word = vmcs_read32(VMX_INSTRUCTION_INFO);
+	if (vmx_insn_info_extended()) {
+		insn.extended   = true;
+		insn.info.dword = vmcs_read64(EXTENDED_INSTRUCTION_INFO);
+	} else {
+		insn.extended  = false;
+		insn.info.word = vmcs_read32(VMX_INSTRUCTION_INFO);
+	}
 
 	return insn;
 }
@@ -413,7 +427,10 @@ static __always_inline unsigned long vmx_get_exit_qual(struct kvm_vcpu *vcpu)
 
 static inline int vmx_get_exit_qual_gpr(struct kvm_vcpu *vcpu)
 {
-	return (vmx_get_exit_qual(vcpu) >> 8) & 0xf;
+	if (vmx_insn_info_extended())
+		return (vmx_get_exit_qual(vcpu) >> 8) & 0x1f;
+	else
+		return (vmx_get_exit_qual(vcpu) >> 8) & 0xf;
 }
 
 static __always_inline u32 vmx_get_intr_info(struct kvm_vcpu *vcpu)
